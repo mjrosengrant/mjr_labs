ec6f822836643710e0fb08f6fa615db9b71d092c
Mike <mjrosengrant@yahoo.com>
2016-09-06 19:37:24 -0400

    Postgres Migration

--- mjrlabs/settings.py
+++ mjrlabs/settings.py
@@ -79,7 +79,7 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ddpuqnk2p41f1g',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'sexvfaxvprttqe',
