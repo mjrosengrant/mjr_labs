fd5b272fd767897b4242297c7c54d632ac6ced0e
Mike <mjrosengrant@yahoo.com>
2016-09-06 20:59:42 -0400

    Securing For Open Source Release

--- blog/urls.py
+++ blog/urls.py
@@ -5,23 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     url(r'^$', views.index),
-
-	url(r'^post/(?P<slug>[^\.]+)', 
-	    views.view_post, 
-	    name='view_blog_post'
-    ),
-
-	url(r'^category/(?P<slug>[^\.]+)', 
-		views.view_category, 
-	    name='view_blog_category'
-    ),
-
-	url(r'^portfolio/', 
-		views.view_portfolio, 
-	    name='view_blog_portfolio'
-    ),
-	url(r'^wysiwyg/', 
-		views.add_post, 
-	    name='add_blog_post'
-    ),
+	url(r'^post/(?P<slug>[^\.]+)', views.view_post, name='view_blog_post'),
+	url(r'^category/(?P<slug>[^\.]+)', views.view_category, name='view_blog_categories')
+	url(r'^portfolio/',views.view_portfolio, name='view_blog_portfolio'),
+	url(r'^wysiwyg/', views.add_post, name='add_blog_post'),
 ]
