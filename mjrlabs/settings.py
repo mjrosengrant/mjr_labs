9937979adf636c753e093e3e549f253700974ee7
Mike <mjrosengrant@yahoo.com>
2016-09-09 18:30:33 -0400

    Securing For Open Source Release

--- mjrlabs/settings.py
+++ mjrlabs/settings.py
@@ -14,9 +14,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Added to try and resolve static issues
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -79,6 +77,10 @@ WSGI_APPLICATION = 'mjrlabs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+# Added to try and resolve static issues
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 DATABASES = {
     'default': {
