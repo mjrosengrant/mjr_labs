5df94f19edab72cf5d805c98291207839689aea8
Mike <mjrosengrant@yahoo.com>
2016-09-09 19:12:49 -0400

    Changing database credentials

--- mjrlabs/settings.py
+++ mjrlabs/settings.py
@@ -83,7 +83,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 DATABASES = {
     'default': {
-        'ENGINE': '',
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.        'NAME': 'ddpuqnk2p41f1g', # Or path to database file if using sqlite3.
         'NAME': '', # Or path to database file if using sqlite3.        
         # The following settings are not used with sqlite3:
         'USER': '',
@@ -99,6 +99,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
