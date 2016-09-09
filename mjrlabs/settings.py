4780b7a30b8afe075424f0baa55cbbd5c4735e53
Mike <mjrosengrant@yahoo.com>
2016-09-09 18:10:07 -0400

    Securing For Open Source Release

--- mjrlabs/settings.py
+++ mjrlabs/settings.py
@@ -14,6 +14,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Added to try and resolve static issues
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
