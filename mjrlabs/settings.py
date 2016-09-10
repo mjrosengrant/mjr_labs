216277be1b31ec55add8f4482ebf23239f6a8af4
Mike <mjrosengrant@yahoo.com>
2016-09-09 21:11:34 -0400

    Fixing Static Files

--- mjrlabs/settings.py
+++ mjrlabs/settings.py
@@ -78,13 +78,9 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Added to try and resolve static issues
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
