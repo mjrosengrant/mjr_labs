716ec98ef1a7c9b4ccca47e7b84ede43ec83d51e
Mike <mjrosengrant@yahoo.com>
2016-12-18 15:48:57 -0500

    Fixes

--- crypto/settings.py
+++ crypto/settings.py
@@ -1,8 +1,8 @@
 {
 	# Key settings
-	"keys": ["personal.gpg"],
+	"keys": [""],
 	"public_keyserver" = "keyserver.ubuntu.com"
-	"email": "mjrosengrant@gmail.com"
+	"email": ""
 	"public_fingerprint":"A41C 52DF F392 0973 82B2  A7CE AAB3 679C 9AF4 32CB"
 	
 
